0200ba01248ebef608d046181aa5814def5cb488
ziv9529 <ziva9529@gmail.com>
2022-10-17 14:48:32 +0300

    projectsUpdate

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,38 +1,44 @@
 import Work1 from "../../assets/work1.png";
 import Work2 from "../../assets/work2.png";
+import Work3 from "../../assets/work3.png";
+import Work4 from "../../assets/work4.png";
+import Work5 from "../../assets/work5.png";
 
 export const projectsData = [
     {
         id: 1,
-        image: Work1,
-        title: 'Online Super Market',
-        technologies: 'Angular, Node.JS, MySQL, Docker.',
-        gh_link: 'https://github.com/ziv9529/project4_shopping'
-    },
-    {
-        id: 2,
         image: Work2,
         title: 'Vacations Feed',
         technologies: 'React, Node.JS, MySQL, Docker.',
-        gh_link: 'https://github.com/ziv9529/vacations'
+        gh_link: 'https://github.com/ziv9529/Vacations-Feed'
+    },
+    {
+        id: 2,
+        image: Work1,
+        title: 'Online Super Market',
+        technologies: 'Angular, Node.JS, MySQL, Docker.',
+        gh_link: 'https://github.com/ziv9529/Online-SuperMarket'
     },
     {
         id: 3,
-        image: Work1,
-        title: 'Work 3',
-        technologies: 'React, React, React, React, React, React'
+        image: Work4,
+        title: 'Crypto Now',
+        technologies: 'JavaScript, Rest API, CSS.',
+        gh_link: 'https://github.com/ziv9529/Crypto-Now'
     },
     {
         id: 4,
-        image: Work1,
-        title: 'Work 4',
-        technologies: 'React'
+        image: Work3,
+        title: 'Bank Management',
+        technologies: 'Angular, Node.JS, MongoDB, Docker.',
+        gh_link: 'https://github.com/ziv9529/Bank-Management'
     },
     {
         id: 5,
-        image: Work1,
-        title: 'Work 5',
-        technologies: 'React'
+        image: Work5,
+        title: 'Servers Management',
+        technologies: 'React, Node.JS, MySQL.',
+        gh_link: 'https://github.com/ziv9529/Servers-Management'
     }
 
 ]
--- src/components/work/WorksItems.jsx
+++ src/components/work/WorksItems.jsx
@@ -6,8 +6,8 @@ const WorksItems = ({ item }) => {
             <img src={item.image} alt="" className='work__img' />
             <h3 className="work__title">{item.title}</h3>
             <h2 className="work__subtitle">Technologies: {item.technologies}</h2>
-            <a href="#" className="work__button">
-                Demo <i className="bx bx-right-arrow-alt work__button-icon"></i>
+            <a href={item.gh_link} alt="https://github.com/ziv9529" target='_blank' rel="noreferrer" className="work__button">
+                Dive into the code <i className="bx bx-right-arrow-alt work__button-icon"></i>
             </a>
         </div>
     )
