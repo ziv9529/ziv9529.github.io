7f761290b4a9dcd9c074559dd59431a52f7d5da1
ziv9529 <ziva9529@gmail.com>
2023-02-12 15:55:31 +0200

    update portfilio

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -4,45 +4,61 @@ import Work3 from "../../assets/work3.png";
 import Work4 from "../../assets/work4.png";
 import Work5 from "../../assets/work5.png";
 import Work6 from "../../assets/work6.png";
+import Work7 from "../../assets/work7.png";
+import Work8 from "../../assets/work8.png";
 
 export const projectsData = [
     {
         id: 1,
+        image: Work7,
+        title: 'AI Image Community',
+        technologies: 'MongoDB, Express, ReactJS, Node.JS',
+        gh_link: 'https://ziv-ai-community.netlify.app/'
+    },
+    {
+        id: 2,
         image: Work2,
         title: 'Vacations Feed',
         technologies: 'React, Node.JS, MySQL, Docker.',
         gh_link: 'https://github.com/ziv9529/Vacations-Feed'
     },
     {
-        id: 2,
+        id: 3,
         image: Work1,
         title: 'Online Super Market',
         technologies: 'Angular, Node.JS, MySQL, Docker.',
         gh_link: 'https://github.com/ziv9529/Online-SuperMarket'
     },
     {
-        id: 3,
+        id: 4,
+        image: Work8,
+        title: 'Daily Soccer Highlights',
+        technologies: 'React.JS, Rest API, Javascript, CSS.',
+        gh_link: 'https://github.com/ziv9529/daily-soccer-highlights'
+    },
+    {
+        id: 5,
         image: Work4,
         title: 'Crypto Now',
         technologies: 'JavaScript, Rest API, CSS.',
         gh_link: 'https://github.com/ziv9529/Crypto-Now'
     },
     {
-        id: 4,
+        id: 6,
         image: Work3,
         title: 'Bank Management',
         technologies: 'Angular, Node.JS, MongoDB, Docker.',
         gh_link: 'https://github.com/ziv9529/Bank-Management'
     },
     {
-        id: 5,
+        id: 7,
         image: Work5,
         title: 'Servers Management',
         technologies: 'React, Node.JS, MySQL.',
         gh_link: 'https://github.com/ziv9529/Servers-Management'
     },
     {
-        id: 6,
+        id: 8,
         image: Work6,
         title: 'Taskbar',
         technologies: 'JavaScript, CSS',
