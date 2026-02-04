6298fa0195dc6d7de5967dddae9b524d9b03d3aa
ziv9529 <ziva9529@gmail.com>
2026-02-04 02:12:30 +0200

    fix: homepage bug, new cv content+file

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -1,25 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const Info = () => {
   return (
-    <div className='about__info grid' >
+    <div className="about__info grid">
       <div className="about__box">
         <i className="bx bx-book-open about__icon"></i>
 
-        <h3 className="about__title"> self-learner</h3>
+        <h3 className="about__title">3+ Years</h3>
+        <h4 className="about__subtitle">Experience</h4>
       </div>
       <div className="about__box">
         <i className="bx bx-briefcase about__icon"></i>
 
-        <h3 className="about__title"> responsible</h3>
+        <h3 className="about__title">6 Engineers</h3>
+        <h4 className="about__subtitle">Led</h4>
       </div>
       <div className="about__box">
         <i className="bx bx-award about__icon"></i>
 
-        <h3 className="about__title">loyalty</h3>
+        <h3 className="about__title">99.9%</h3>
+        <h4 className="about__subtitle">Uptime</h4>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Info
+export default Info;
